4556394f53fbdb957433b0a0bb1e578e9bf683d7
Fraol <fraolm41@gmail.com>
2026-02-18 07:15:42 +0300

    feat: configure simplejwt + add basic token obtain/refresh endpoints

---
+++ encore/config/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # JWT endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),      # login
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),     # refresh access token
+    
+    # We'll add register later
+]

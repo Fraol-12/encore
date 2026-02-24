d3a9c5eb7e6a3fa21f723b5a25d71f8bb6a1420e
Fraol <fraolm41@gmail.com>
2026-02-25 00:17:58 +0300

    feat(config): add sync url

--- encore/config/urls.py
+++ encore/config/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from apps.users.views import RegisterView
 from rest_framework.routers import DefaultRouter 
-from apps.playlists.views import PlaylistViewSet 
+from apps.playlists.views import PlaylistViewSet, SyncOperationDetailView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api/register/', RegisterView.as_view(), name='register'),
     
     path('api/', include(router.urls)), 
+    path('api/sync-operations/<int:pk>/', SyncOperationDetailView.as_view(), name='sync-operation-detail'),
 ]

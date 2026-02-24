8aae8485806ec985d672824f7266d7273a780281
Fraol <fraolm41@gmail.com>
2026-02-24 07:51:55 +0300

    feat: Integrate youtube import into playlist creation flow

--- encore/apps/playlists/serializers.py
+++ encore/apps/playlists/serializers.py
@@ -8,9 +8,22 @@ class PlaylistSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'title', 'description', 'youtube_playlist_id',
             'spotify_playlist_id', 'sync_status', 'source_status',
-            'last_synced_at', 'created_at', 'updated_at'
+            'youtube_channel_title', 'youtube_item_count', 'youtube_thumbnail_url',
+            'youtube_published_at', 'youtube_last_fetched_at', 'youtube_privacy_status',
+            'created_at', 'updated_at'
         ]
-        read_only_fields = ['id', 'sync_status', 'source_status', 'last_synced_at', 'created_at', 'updated_at']
+        read_only_fields = [
+            'id', 'sync_status', 'source_status',
+            'youtube_channel_title', 'youtube_item_count', 'youtube_thumbnail_url',
+            'youtube_published_at', 'youtube_last_fetched_at', 'youtube_privacy_status',
+            'created_at', 'updated_at'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Make title optional for input (we fill it from YouTube)
+        self.fields['title'].required = False
+        self.fields['title'].allow_blank = True
 
 class PlaylistItemSerializer(serializers.ModelSerializer):
     class Meta:        
@@ -20,4 +33,4 @@ class PlaylistItemSerializer(serializers.ModelSerializer):
             'duration_seconds', 'thumbnail_url', 'position',
             'is_removed_from_source', 'added_at'
         ]
-        read_only_fields = fields
+        read_only_fields = fields 
